a6dfa153683124fab63c9d135246e03c0af68153
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2024-10-04 11:10:47 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,9 @@
-.
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+}
+
+const tickConfing = {
+    color: getCSS ('--primary-color'),
+    size: 16,
+    family: getCSS ('--font')
+}
