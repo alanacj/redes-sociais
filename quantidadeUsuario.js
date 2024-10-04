9d7344e78d126a7716a1925f271c47de25f02594
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2024-10-04 11:12:43 -0300

    Update quantidadeUsuario.js

--- quantidadeUsuario.js
+++ quantidadeUsuario.js
@@ -11,15 +11,43 @@ async function quantidadeUsuariosPorRede() {
             y: quantidadeDeUsuarios, 
             type: 'bar',
             marker: {
-                color: getComputedStyle(document.body).getPropertyValue('--primary-color')
+                color: getCSS ('--primary-color')
             }
         }
     ]
 
+    const layout = {
+        plot_bgcolor: getCSS ('--bg-color'),
+        paper_bgcolor: getCSS ('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font: {
+                color: getCSS ('--primary-color'),
+                family: getCSS ('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfing,
+            title: {
+                text: 'nomes das redes sociais',
+                font: getCSS('--secundary-color')
+            }
+        },
+        yaxis: {
+            tickfont: tickConfing,
+            title: {
+                text: 'bilhôes de usuários ativos',
+                font: getCSS('--secundary-color')
+            }
+        },
+    }
+
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuariosPorRede()
