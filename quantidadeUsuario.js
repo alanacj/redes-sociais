719cf61f326bc368a9096363fec5a7b61369ecad
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2024-10-25 08:27:12 -0300

    Update quantidadeUsuario.js

--- quantidadeUsuario.js
+++ quantidadeUsuario.js
@@ -44,10 +44,7 @@ async function quantidadeUsuariosPorRede() {
         },
     }
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
+    criarGrafico(data,layout)
 }
 
 quantidadeUsuariosPorRede()
