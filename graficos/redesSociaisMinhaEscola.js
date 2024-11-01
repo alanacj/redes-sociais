1331d0a4c7afe361eea537018905d5c2ee690e06
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2024-11-01 11:11:10 -0300

    Create redesSociaisMinhaEscola.js

---
+++ graficos/redesSociaisMinhaEscola.js
@@ -0,0 +1,7 @@
+async function redesSociaisFavoritaMinhaEscola () {
+    const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPo6HS3jnJY9NNhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1nSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+    const res = await fetch (url)
+    const dados = await res.json ()
+}
+
+redesSociaisFavoritaMinhaEscola ()
